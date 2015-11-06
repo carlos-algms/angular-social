4f1f4cf6d4ed030070c03f6eb203eef338c96dea
Carlos A. Gomes <carlos.gomes@s2it.com.br>
2015-11-06 17:17:30 -0200

    deleted some comments

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name angularTinySocialApp
- * @description
- * # angularTinySocialApp
- *
- * Main module of the application.
- */
 angular
   .module('angularTinySocialApp', [
     'ngAnimate',
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,6 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name angularTinySocialApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the angularTinySocialApp
- */
+
 angular.module('angularTinySocialApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
